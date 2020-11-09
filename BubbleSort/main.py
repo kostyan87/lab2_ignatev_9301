9c479203f0a84924cbaf0dc8d1d0b33cfb6ae439
kostyan <kostya.ignatev.14@mail.ru>
2020-11-09 16:53:07 +0300

    Полностью сделана и протестирована пузырьковая сортировка

--- BubbleSort/main.py
+++ BubbleSort/main.py
@@ -3,21 +3,24 @@
 
 def BubbleSort(array, typeOfSort):
    
-   if type(array) is not list: raise Exception('"array" is not array')
-   if type(typeOfSort) is not int: raise Exception('"item" is not integer')
+   if type(array) is not list: raise Exception('Array is not array')
+   if type(typeOfSort) is not int: raise Exception('Type of sort is not integer')
    if (typeOfSort < 0) or (typeOfSort > 1): raise Exception("Incorrect type of sort")
 
+   border = len(array) - 1
    if typeOfSort == 0:
       for i in range(0, len(array) - 1):
-         for i in range(0, len(array) - 1):
+         for i in range(0, border):
             if array[i] < array[i + 1]:
                array[i], array[i + 1] = array[i + 1], array[i]
+         border -= 1
    else:
       for i in range(0, len(array) - 1):
-         for i in range(0, len(array) - 1):
+         for i in range(0, border):
             if array[i] > array[i + 1]:
                array[i], array[i + 1] = array[i + 1], array[i]
+         border -= 1
    
    return array
 
-print(BubbleSort([1, 3, 0, 7, 5, 3, 88, 14, 12], 1))
+print(BubbleSort([1, 3, 3, 8, 4, 78, 66, 10, 10], 0))
